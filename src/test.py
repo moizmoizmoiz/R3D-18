850aa924dd38888bbf02cea2f3e6d3d010493577
dpsn1999 <ds22342@gmail.com>
2023-05-14 16:46:44 +0100

    Updated test function to include conf matrix

--- src/test.py
+++ src/test.py
@@ -4,7 +4,11 @@ import torch
 import torch.nn.functional as F
 
 
-def test(model, dataloader, device):
+##define test function
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def test(Rmodel, dataloader, device):
     # meters
     loss_meter = AverageMeter()
     top1_acc_meter = AverageMeter()
@@ -12,19 +16,22 @@ def test(model, dataloader, device):
     # switch to test mode
     correct_top1 = 0
     correct_top5 = 0
-    model.eval()
+    y_true = []
+    y_pred = []
+    Rmodel.eval()
     tk = tqdm(dataloader, total=int(len(dataloader)), desc='Test', unit='frames', leave=False)
     for batch_idx, data in enumerate(tk):
         # fetch the data
         frame, label = data[0], data[1]
-        # after fetching the data transfer the model to the
+        y_true.extend(label.cpu().numpy())
+        # after fetching the data transfer the model to the 
         # required device, in this example the device is gpu
-        # transfer to gpu can also be done by
+        # transfer to gpu can also be done by 
         frame, label = frame.to(device), label.to(device)
-        # since we don't need to back propagate loss in testing,
-        # we don't keep the gradient
+        # since we dont need to backpropagate loss in testing,
+        # we dont keep the gradient
         with torch.no_grad():
-            output = model(frame)
+            output = Rmodel(frame)
         # compute the loss function just for checking
         loss_this = F.cross_entropy(output, label)
         # get the index of the max log-probability
@@ -35,9 +42,9 @@ def test(model, dataloader, device):
         correct_top1 += correct_this_top1
         # compute top-1 accuracy
         acc_this_top1 = correct_this_top1 / label.shape[0] * 100.0
-        # update the top-1 accuracy meter
+        # update the top-1 accuracy meter 
         top1_acc_meter.update(acc_this_top1, label.shape[0])
-
+        
         # compute top-5 accuracy
         _, pred_top5 = torch.topk(output, k=5, dim=1)
         pred_top5 = pred_top5.t()
@@ -45,18 +52,13 @@ def test(model, dataloader, device):
         correct_top5 += correct_this_top5
         acc_this_top5 = correct_this_top5 / label.shape[0] * 100.0
         top5_acc_meter.update(acc_this_top5, label.shape[0])
+        
+        y_pred.extend(pred.cpu().numpy())
 
-        # update the loss meter
+        # update the loss meter 
         loss_meter.update(loss_this.item(), label.shape[0])
+
     print('Test: Average loss: {:.4f}, Top-1 Accuracy: {}/{} ({:.2f}%), Top-5 Accuracy: {}/{} ({:.2f}%)\n'.format(
-        loss_meter.avg, correct_top1, len(dataloader.dataset), top1_acc_meter.avg, correct_top5,
-        len(dataloader.dataset), top5_acc_meter.avg))
-
-
-def get_confusion_matrix(pred, label, num_classes):
-    conf_mat = torch.zeros(num_classes, num_classes)
-    for i in range(len(pred)):
-        actual_class = int(label[i])
-        predicted = int(pred[i])
-        conf_mat[actual_class][predicted] += 1
-    return conf_mat
+        loss_meter.avg, correct_top1, len(dataloader.dataset), top1_acc_meter.avg, correct_top5, len(dataloader.dataset), top5_acc_meter.avg))
+    print('Confusion Matrix:')
+    print(confusion_matrix(y_true, y_pred))
