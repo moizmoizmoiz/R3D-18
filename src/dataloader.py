afc8305e092cad884692ea506f84e90cfb35584c
Moiz <57187188+moizmoizmoiz@users.noreply.github.com>
2023-05-19 02:21:30 +0100

    fix

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,7 +3,7 @@ import torchvision.transforms as transforms
 import torch
 from PIL import Image
 import os
-from tqdm import tqdm_notebook as tqdm
+from tqdm.notebook import tqdm
 
 class VideoDataset(torch.utils.data.Dataset):
     def __init__(self, root_dir, frames_per_clip=16, transform=None):
