07b4d09824aad02cae555ab43db3f281c0efe75d
Moiz <57187188+moizmoizmoiz@users.noreply.github.com>
2023-05-19 01:59:45 +0100

    fix

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,7 +3,6 @@ import torchvision.transforms as transforms
 import torch
 from PIL import Image
 import os
-from tqdm import tqdm
 
 
 class VideoDataset(torch.utils.data.Dataset):
@@ -14,15 +13,16 @@ class VideoDataset(torch.utils.data.Dataset):
 
         self.classes = os.listdir(root_dir)
         self.class_to_idx = {c: i for i, c in enumerate(self.classes)}
+
         # following loops through each class directory and video file in the dataset, and extract a clip of frames from each video.
-        # For each frame in the clip, it loads the image from file using the Image class from the Python Imaging Library (PIL),
-        # applies the optional transform, and adds it to the clip. If the clip has at least frames_per_clip frames,
-        # it is added to a list called clips along with the class name
+        #For each frame in the clip, it loads the image from file using the Image class from the Python Imaging Library (PIL),
+        #applies the optional transform, and adds it to the clip. If the clip has at least frames_per_clip frames,
+        #it is added to a list called clips along with the class name
         print('Transforming Data...')
-        print(f"Total Classes = {len(self.classes)}")
+        print(f"Total Classes = {len(self.classes)}" )
         self.clips = []
-        for class_name in tqdm(self.classes):
-            print(f"\nFinished Transforming: {class_name}")
+        for class_name in self.classes:
+            print('working on: '+class_name)
             class_dir = os.path.join(root_dir, class_name)
             for video_file in os.listdir(class_dir):
                 video_path = os.path.join(class_dir, video_file)
@@ -31,6 +31,7 @@ class VideoDataset(torch.utils.data.Dataset):
                     frame_path = os.path.join(video_path, frame_file)
                     frame = Image.open(frame_path)
                     if self.transform:
+                        print('Transforming video: '+frame_file)
                         frame = self.transform(frame)
                     clip.append(frame)
                 if len(clip) >= frames_per_clip:
@@ -38,12 +39,12 @@ class VideoDataset(torch.utils.data.Dataset):
         print('Done Transformation.')
 
     def __len__(self):
-        return len(self.clips)  # returns the number of clips in the dataset
+        return len(self.clips) #returns the number of clips in the dataset
 
     def __getitem__(self, idx):
         clip, class_name = self.clips[idx]
         clip = torch.stack(clip, dim=0)
         label = self.class_to_idx[class_name]
         return clip, label
-        # returns the idx-th clip in the dataset, along with its label.
-        # The clip is returned as a tensor with dimensions (frames_per_clip, C, H, W), where C, H, and W are the number of channels, height, and width of each frame, respectively
+        #returns the idx-th clip in the dataset, along with its label.
+        #The clip is returned as a tensor with dimensions (frames_per_clip, C, H, W), where C, H, and W are the number of channels, height, and width of each frame, respectively
