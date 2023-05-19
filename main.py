5c1959a648473d7e2a4f7fbfefb9b8f14594b3b8
Moiz <57187188+moizmoizmoiz@users.noreply.github.com>
2023-05-19 10:40:29 +0100

    fix

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def main():
     elapsed = round(time.time() - time_start)
     elapsed = str(datetime.timedelta(seconds=elapsed))
     print(f"Elapsed {elapsed}")
-    writer.add_text(f"Elapsed {elapsed}")
+    writer.add_text("Elapsed", {elapsed})
 
     writer.close()
 
